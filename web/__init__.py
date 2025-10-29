82f041e92ce5ce93a7b07451fa444d5642d73276
Chacha Brian <braverachacha@email.com>
2025-10-30 02:00:36 +0300

    Working Flask app ready for Vercel deployment

--- web/__init__.py
+++ web/__init__.py
@@ -1,27 +1,32 @@
 from flask import Flask
 from flask_login import LoginManager
 from web.models import db, User
+import os
 
 def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'bomsjdj jdo'
-  
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-  db.init_app(app)
-  
-  from .views import views
-  from .auth import auth
-  
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
-  
-  login_manager = LoginManager()
-  login_manager.login_view = 'auth.login'
-  login_manager.init_app(app)
-  
-  @login_manager.user_loader
-  def load_user(id):
-    return User.query.get(int(id))
-  
-  return app
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'chacha bomsjdj jdo bravera254'
+    
+    if 'DATABASE_URL' in os.environ:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    return app
