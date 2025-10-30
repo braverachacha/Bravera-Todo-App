4b3e65cde5af719e1915e2817bbd6e36638c1b58
Chacha Brian <braverachacha@email.com>
2025-10-30 03:01:05 +0300

    Fix: Neon PostgreSQL config and SSL support

--- web/__init__.py
+++ web/__init__.py
@@ -6,27 +6,30 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'chacha bomsjdj jdo bravera254'
-    
-    if 'DATABASE_URL' in os.environ:
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    
+
+    database_url = os.getenv('DATABASE_URL', 'sqlite:///database.db')
+
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
-    
+
+
     from .views import views
     from .auth import auth
-    
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-    
+
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-    
+
     return app
